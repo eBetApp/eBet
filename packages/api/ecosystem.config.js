d2b4613ea5c747c8f6b884e42ee112e06357dc3b
BLECUONA <43953041+BLECUONA@users.noreply.github.com>
2020-04-19 15:37:06 +0200

    api[PROCESS](heroku)Added auto deployment of api lerna package (#1)
    * api[PROCESS](heroku) try to deploy subFolder
    
    * api[PROCESS](heroku) added buildpacks on heroku and remove procfile from root
    
    * api[PROCESS](heroku) added --prefix in start script
    
    * api[PROCESS](heroku) try with git push subtree
    
    * api[PROCESS](heroku) try with subdir-heroku-buildpack

--- packages/api/ecosystem.config.js
+++ packages/api/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'app',
-			script: './.dist/main.js',
+			script: '.dist/main.js',
 
 			// Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
 			args: 'one two',
