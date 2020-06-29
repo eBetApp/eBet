8f6462553c7915da3ce0d2c3de92c077890c59f2
BLECUONA <benjaminlecuona@gmail.com>
2020-06-29 13:08:11 +0200

    app[DEV](UI) added safeArea for iOS

--- packages/app/src/components/styled/Views.tsx
+++ packages/app/src/components/styled/Views.tsx
@@ -1,17 +1,22 @@
 import React, { useContext } from "react";
 import { View, KeyboardAvoidingView, Platform } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export function MainView(props) {
-  return <View {...props} style={{ ...props.style, margin: 5 }} />;
+  return (
+    <SafeAreaView {...props} style={{ ...props.style, flex: 1, margin: 5 }} />
+  );
 }
 
 export function MainKeyboardAvoidingView(props) {
   return (
-    <KeyboardAvoidingView
-      {...props}
-      behavior={Platform.OS == "ios" ? "padding" : "padding"}
-      keyboardVerticalOffset={Platform.OS == "android" ? -700 : 0}
-      style={{ ...props.style, margin: 5 }}
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        {...props}
+        behavior={Platform.OS == "ios" ? "padding" : "padding"}
+        keyboardVerticalOffset={Platform.OS == "android" ? -700 : 0}
+        style={{ ...props.style, margin: 5 }}
+      />
+    </SafeAreaView>
   );
 }
